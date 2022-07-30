1a6bc2f621ff0963df3f3082c1433cb5f9d25485
mohamad_herorist <107700829+3uendu3ue@users.noreply.github.com>
2022-07-30 21:32:12 +0430

    herorist.py

--- herorist.py
+++ herorist.py
@@ -1,32 +1,43 @@
+#import
 import os
-#What do you have in the source?
-os.system("pip install colored")
-os.system("pip install colorama")
-os.system("pip install datetime")
-#importing
 import time
 import sys
-import colored
-import colorama
 import datetime
-from colored import fg, bg, attr
-from colorama import Fore, Back, Style
-# colors
-red='\033[31m'
-green='\033[32m'
-blue='\033[36m'
-pink='\033[35m'
-rang='\033[34m'
+import requests
+
+
+# code colors
+red='\033[31m'  #قرمز
+green='\033[32m' #سبز
+blue='\033[36m' #ابی
+pink='\033[35m'  #بنفش
+rang='\033[34m' #ابی تیره
+yellow='/033[93m'  #زرد
+
+#dat
+dat = (datetime.datetime.today())
+yellow,green="\033[93m","\033[92m"
+
+
+#print_mohamad.herorist
+print(f"{yellow} ")
+banner = (f"""  MOHAMAD HERORIST GOD FILTERING""")
+for bnr in banner:
+        sys.stdout.write(bnr)
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+
+#clear
 os.system("clear")
+print()
+      
+            
+#baner
 print(f"{red} ")
-print(Fore.WHITE + "")
-#date
-x = str(datetime.datetime.now())
-print("started " + (x))
-print(f"{green} ")
-print(" cod filtering ")
-print(Fore.YELLOW + "")
-x = """ Loading ~ ~ ~
+banner = (f"""      
+
+                                               
                ..:::::::::..
            ..:::aad8888888baa:::..
         .::::d:?88888888888?::8b::::.
@@ -47,100 +58,120 @@ x = """ Loading ~ ~ ~
       `::::::::88::88::88:::88::::::::'
         `::::::88::88::88:::88::::::'
            ``:::::::::::::::::::''
-                ``:::::::::::""
-                
-_ __ ___   ___ | |__   __ _ _ __ ___   __ _  __| |
-| '_ ` _ \ / _ \| '_ \ / _` | '_ ` _ \ / _` |/ _` |
-| | | | | | (_) | | | | (_| | | | | | | (_| | (_| |
-|_| |_| |_|\___/|_| |_|\__,_|_| |_| |_|\__,_|\__,_|
+                ``:::::::::::""")
+for bnr in banner:
+        sys.stdout.write(bnr)
+        sys.stdout.flush()
+        time.sleep(0.004)
 
+
+        
+print("")        
+print("")        
+print("")        
+print("")        
+        
+        
+print(f"{rang} ")
+banner = (f"""{yellow}《{dat}
+ {rang}                 
+ _ __ ___   ___ | |__   __ _ _ __   __ _  __| |
+| '_ ` _ \ / _ \| '_ \ / _` | '_ \ / _` |/ _` |
+| | | | | | (_) | | | | (_| | | | | (_| | (_| |
+|_| |_| |_|\___/|_| |_|\__,_|_| |_|\__,_|\__,_|
+
+                                  
 | |__   ___ _ __ ___  _ __(_)___| |_
 | '_ \ / _ \ '__/ _ \| '__| / __| __|
 | | | |  __/ | | (_) | |  | \__ \ |_
-|_| |_|\___|_|  \___/|_|  |_|___/\__|                
-                
-                
-"""
-for c in x:
-        sys.stdout.write(c)
+|_| |_|\___|_|  \___/|_|  |_|___/\__|
+   """)
+for bnr in banner:
+        sys.stdout.write(bnr)
         sys.stdout.flush()
-        time.sleep(0.004)
-print(f"{blue} ")
-print("#.........")
-time.sleep(0.9)
-print("##........")
-time.sleep(0.9)
-print("###.......")
-time.sleep(0.8)
-print("####......")
-time.sleep(0.7)
-print("#####.....")
-time.sleep(0.6)
-print("######....")
-time.sleep(0.5)
-print("#######...")
-time.sleep(0.4)
-print("########..")
-time.sleep(0.3)
-print("#########.")
-time.sleep(0.2)
-print("##########")
-time.sleep(0.1)
-print(f"{pink} ")
-print("Play - - - ! ")
-print(Fore.YELLOW + "")
+        time.sleep(0.009)
+
+        
+        
+            
+#start
+print("")
+print("")
+print("")
+print("")
 time.sleep(1.7)
-print("  abar filtering (keristofer) ")
-print(" ")
-print("   chanel rubika = @aqa_keristofer  ")
-print(" ")
-print(f"{green} ")
-print(" cod server ")
+print ("🖤")
+time.sleep(0.5)
+print ("🖤🖤")
+time.sleep(0.4)
+print ("🖤🖤🖤")
+time.sleep(0.3)
+print ("🖤🖤🖤🖤")
+time.sleep(0.2)
+print ("🖤🖤🖤🖤🖤")
 time.sleep(0.1)
-print("server1 = y//d/f")
-time.sleep(1.0)
-print("server2 = g/d//")
+print ("🖤🖤🖤🖤🖤🖤")
+time.sleep(0.5)
+print ("🖤🖤🖤🖤🖤🖤🖤")
 time.sleep(0.1)
-print(" Dxprit ")
-time.sleep(1.0)
-print("Dxprit = http://Dxprit.ngopb.hackers65.7678729986.infect.in ")
+print ("🖤🖤🖤🖤🖤🖤🖤🖤")
+time.sleep(0.5)
+print("🖤🖤🖤🖤🖤🖤🖤🖤🖤")
 time.sleep(0.1)
-print(f"{red} ")
-up = input("""
-《 cod filter account 1 》
-《 cod filter gap 2 》
-《 cod filter chanel 3 》
- Which do you want >>> """)
-print("  ")
-print(f"{blue} ")
-time.sleep(2.0)
+
+print("")
+print("")
+
+
+#input
+up = input(""" 
+[cood filter Account rubika (1) ]
+[cood filter channel rubika (2) ]
+[cood filtr group rubika    (3) ]         
+
+Which do you want>>>""")
+
+
+#!
+print (blue)
+print("\rscanning..",end="",flush=False)
+time.sleep(1)
+print("\rscanning....",end="",flush=False)
+time.sleep(1)
+print("\rscanning.....",end="",flush=False)
+time.sleep(1)
+print("\rscanning......",end="",flush=False)
+time.sleep(1)
+print("\rscanning.......",end="",flush=False)
+time.sleep(0.5)
+print("\rscanning........",end="",flush=False)
+time.sleep(0.5)
+print("\rscanning.........",end="",flush=False)
+time.sleep(0.5)
+print("\rscanning..........",end="",flush=False)
+time.sleep(1)
+
+
+print("")
+print("")
+print("")
+
+
+
+#cod filteri acoonut
 if up == "1":
-        print(' cod account = (https://ter.af-ter.af-ter.(11.36.37.000)https://kode.code(/YtTk_018.)(3.1.6.8/)rubika.fill.robika.fyll.*(Af-aF-Af/af+fill+by+rubika+*+*+http://fil_RuBiKa_amirhossein_s_hack_63.78.28.111_fill/rubika/(http.ir)22.495.658_fill&( 11.36.37.000)http://fill_RUBIKA/http://6.0.3.6/g///f//i///h.6.0.3.2.3.8.6.4.5.8.6.3.7.7.9.0.5.5.1.2.4.0.3.4(*7802_027_67Filter.30_rub_fil87.917(.rubika_filtery_godrat)(/yftt15k(https://bit.ly/3ild93L)')
-if up != "1":
-        print(' cod filter account ')
+         print ("""  \033[41m(6.0.3.6/d///f//i///y.6.0.3.2.3.8.6.4.5.8.6.3.7.7.9.0.5.5.:bit.ly/3ild93L1.2.4.0.3.4.6.0.3.6.6.0.3.2.3.8.6.4.5.8.6.3.7.7.9.0.5.5.1.2.4.0.3.4)\33[1;0m  """)
+   
+         
+                     
+#cod filteri channel     
 if up == "2":
-        print(' cod gap = (http://1.7.2.5//e/(@supportbot)h//.e.99.61.porn-sex.8.3.(iran-@support)0.5.4.8.9.sport.-rubika.-spam.(/yftt15k)_1.2.46kpd.9.hfdjsh.98.87.80.0.3987.99//Fil//com.5.106.8.151////@netreport.fil.(/yftt15k).sex.pornhub.com)')
-if up != "2":
-        print(' cod filter gap ')
+         print ("""  \033[41m(7.8.9.2.d/f///y//6065///g.k.4.6.7.8.2.3.5.6.7.3.2.9.3.:bit.ly/3ild93L.9.1.5.3.6.2.9.6/.7.8.9.2.4.6.7.8.2.3.5.6.7.3.2.9.3.9.1.5.3.6.2.9.6/)\33[1;0m   """)
+         
+ 
+           
+#cod filteri group                 
 if up == "3":
-        print(' cod chanel = (/9.9.2.3/d//f/hThese.users.are.committing.many.insults.and.insults.to.the.leader.and.the.people.of.Iran.against.the.laws.of.the.Prophet.Rubika.Invites.And.also.threatens.users.to.hack.and.filter.accounts.and.extorts.from.them.filter.acant.filtet.the.acant/5.3.6.8.4.2.6.4.filter.gitapt.4.5.7.3.8.4.6.3/) ')
-if up != "3":
-        print(' cod filter chanel ')
-time.sleep(0.6)
-print("""
-         cod haro mahklot konid !
-""")
-print(f"{pink} ")
-print(" cod ro 10 bar bezanid ke feik nashe ")
-print("♡Thanks♡")
-print(" ip rubika = 5.106.10.226 ")
-print(f"{red} ")
-print(" no love ")
-print(" ")
-print(" abar filtering rubika aqa mohamad herorist ")
-time.sleep(2.0)
-print(" ")
-print(" ostore filtering ")
-print(" herorist ")
-print(" the end ")
-time.sleep(2.0)
+        print("""" \033[41m(4.0.1.4/f.i.d/gl/4.5.3//5.2.5.7.2.6.8.4.2.4.7.9.3.1.5.2.7.9.8.:bit.ly/3ild93L.0.1.3.4.0.1.4.4.5.3.5.2.5.7.2.6.8.4.2.4.7.9.3.1.5.2.7.9.8.0.1.3)\33[1;0m  """)   
+        
+print("dedication mehrad herorist❤️")
