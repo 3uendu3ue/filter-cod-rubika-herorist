64dba7d2bc7544633f53003a858cd78fe4b9af8e
mohamad_herorist <107700829+3uendu3ue@users.noreply.github.com>
2022-07-31 11:42:20 +0430

    herorist.py

--- herorist.py
+++ herorist.py
@@ -1,30 +1,42 @@
+#mi_aryan_megatrn
 import os
-#What do you have in the source?
+print (" ♕ MOHAMAD HERORIST ♕ ")
+# auto install libs
 os.system("pip install colored")
 os.system("pip install colorama")
 os.system("pip install datetime")
-#importing
-import time
-import sys
+#importing libs
+
+import time                                                     
+import sys                                                      
+import colored                                                  
+import colorama                                                 
 import datetime
 from colored import fg, bg, attr
-from colorama import Fore, Back, Style
-# colors
+from colorama import Fore, Back, Style                          
+# code colors
 red='\033[31m'
 green='\033[32m'
 blue='\033[36m'
 pink='\033[35m'
 rang='\033[34m'
+#$CRIPT
+
 os.system("clear")
-print(f"{red} ")
-print(Fore.WHITE + "")
-#date
-x = str(datetime.datetime.now())
-print("started " + (x))
+
 print(f"{green} ")
-print(" cod filtering ")
-print(Fore.YELLOW + "")
-x = """ Loading ~ ~ ~ 
+
+print (Fore.WHITE + "")
+
+#date()
+x = str(datetime.datetime.now())
+
+print("started " + (x))
+
+print(f"{blue} ")
+print ("         Script By")
+print (Fore.BLUE + "")
+x = """ 
                ..:::::::::..
            ..:::aad8888888baa:::..
         .::::d:?88888888888?::8b::::.
@@ -45,13 +57,8 @@ x = """ Loading ~ ~ ~
       `::::::::88::88::88:::88::::::::'
         `::::::88::88::88:::88::::::'
            ``:::::::::::::::::::''
-                ``:::::::::::"""
-for c in x:
-        sys.stdout.write(c)
-        sys.stdout.flush()
-        time.sleep(0.004)        
-print(f"{green} ")
-banner = (f"""
+                ``:::::::::::"
+                
 _ __ ___   ___ | |__   __ _ _ __ ___   __ _  __| |
 | '_ ` _ \ / _ \| '_ \ / _` | '_ ` _ \ / _` |/ _` |
 | | | | | | (_) | | | | (_| | | | | | | (_| | (_| |
@@ -60,170 +67,113 @@ _ __ ___   ___ | |__   __ _ _ __ ___   __ _  __| |
 | |__   ___ _ __ ___  _ __(_)___| |_
 | '_ \ / _ \ '__/ _ \| '__| / __| __|
 | | | |  __/ | | (_) | |  | \__ \ |_
-|_| |_|\___|_|  \___/|_|  |_|___/\__|
-""")
-for bnr in banner:
-        sys.stdout.write(bnr)
-        sys.stdout.flush()
-        time.sleep(0.004)
-print(f"{green} ")
-print ("Play - - - ! ")
-print (Fore.YELLOW + "")
-time.sleep(1.7)
-print(f"{blue} ")
-time.sleep(1.0)
-print ("!!")
-time.sleep(0.7)
-print ("lod....")
-time.sleep(0.7)
-print ("n/y")
-time.sleep(0.6)
-print ("....")
-time.sleep(0.5)
-print ("......")
-time.sleep(0.4)
-print (".........")
-time.sleep(0.3)
-print ("..................")
-time.sleep(0.2)
-print (".........................")
-time.sleep(0.1)
-print (".............................")
-time.sleep(0.5)
-print ("...............................")
-time.sleep(0.1)
-print (".................................")
-time.sleep(0.5)
-print ("....................................")
-time.sleep(0.1)
-print ("~iegn")
-time.sleep(0.5)
-print ("cood info server")
-time.sleep(0.1)
-print ("script server cood")
-time.sleep(0.5)
-print ("server1:y//d/f")
-time.sleep(1.0)
-print ("server2:f//d/")
-time.sleep(0.5)
-print ("server3:g/d//")
-time.sleep(0.1)
-print ("server4:d//f/h")
-time.sleep(0.5)
-print ("server5:f//a/y")
-time.sleep(1.0)
-print ("making script maxtor")
-time.sleep(0.5)
-print ("script making cood filter rubika.apk 2.9.5 80%")
-time.sleep(1.0)
-print ("script server")
-time.sleep(0.5)
-print ("script connect server rubika.ir/rubika.com/ file os server")
-time.sleep(1.0)
-print ("cood filter group 90% apn cood proxy-server")
-time.sleep(0.5)
-print ("cood filter account rubika 87% apn cood filter proxy-port:8000")
-time.sleep(1.0)
-print ("cood filter chaneel rubika 78% apn proxy file port:auth")
-time.sleep(0.5)
+|_| |_|\___|_|  \___/|_|  |_|___/\__|                
+"""
+for c in x:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.02)
 print ("")
-print (" ")
-print ("")
-# 1 chanel 2 Group 3 acc
-up = input(""" 
-[cood filter channel rubika (1) ]
-[cood filtr group rubika    (2) ]                 
-[cood filter Account rubika (3) ]
-[Super   (0) ]
-Which do you want>>>""")
+print(" US ")
 print("")
-print(f"{pink} ")
-time.sleep(3.0)
-print(""" in the name of God 
-_____________
-Loading . . . .
-. . . . . . .  . .
- . . . . . . . . . . 
- 
-💣 💣 💣 💣 💣 💣 💣 💣
-                    
-|>|>|>|>|>|>|>|>|>|>|>|>|>|>|>|>|>|>|
-______________________________
-""")
+print("")
+print("")
+print (" ")
+up = input("""
+          _   Acc 1 _
+            _   Chanel 2 _
+                   _  Gp 3 ~ _>> """ )
+print(f"{blue} ")
 
-    
-if up == "0":
-         print ('(Super) Code filter =>' + '%s%s (https://s4.uupload.ir/files/img_20210728_194736_412_9t6h.jpg)(02.5/1.000)("https://bit.ly/3ild93L")Dxprit.filinger.hacker65.76.infect.in)(https://15/filt.15///h/h.///f.15.til.Haacker.Hacker.Filtrng)(yftt15k)(Obscenity.to/ir/rubika/v2.9.5.v8a*af*fa*Af*Fa*AF*FA)(www.xxnx.com)(https://filltering.rubika.filltr.accunt.rubika.ir) %s' % (fg('red'), bg('black'), attr('reset')))
 
 
 if up == "1":
 
-        print ('code Fil Channel ≠>' + '%s%s *https://ter.af-ter.af-ter.(11.36.37.000)https://kode.code(/YtTk_018.)(3.1.6.8/)rubika.fill.robika.fyll.*(Af-aF-Af/af+fill+by+rubika+*+*+http://fil_RuBiKa_amirhossein_s_hack_63.78.28.111_fill/rubika/(http.ir)22.495.658_fill&( 11.36.37.000)http://fill_RUBIKA/http://6.0.3.6/g///f//i///h.6.0.3.2.3.8.6.4.5.8.6.3.7.7.9.0.5.5.1.2.4.0.3.4(*7802_027_67Filter.30_rub_fil87.917(.rubika_filtery_godrat)(/yftt15k(https://bit.ly/3ild93L) %s' % (fg('red'), bg('yellow'), attr('reset')))
+        print ('Account  == ' + '%s%s (https://.lnpker.dlit.http//(reaprot.(81.23.40.7)_senotor.pro(+bit.ly/9ploftakhttps+×CFh) %s' % (fg('red'), bg('black'), attr('reset')))
 
 
 if up != "1":
-        print("    COD  ")
+        print("    мy code ")
 
 
 if up == "2":
-        print ('code group ≠>' + '%s%s http://1.7.2.5//e/(@supportbot)h//.e.99.61.porn-sex.8.3.(iran-@support)0.5.4.8.9.sport.-rubika.-spam.(/yftt15k)_1.2.46kpd.9.hfdjsh.98.87.80.0.3987.99//Fil//com.5.106.8.151////@netreport.fil.(/yftt15k).sex.pornhub.com %s' % (fg('white'), bg('black'), attr('reset')))
+        print (' Chanel Cod  == ' + '%s%s (http://3.py/F./h.k.p////6.4.(157.621/https://s4.uupload.ir/files/n5_j0e2.jpg)(.(/U/p.[157.6)l////8.9.0.2.4.7.9.3.1.5.2.7.8.3.0.1.0) %s' % (fg('white'), bg('black'), attr('reset')))
 
 
 if up != "2":
-
-        print("     cσ∂s")
-
+    print("    My Cods ")
 
 if up == "3":
 
-        print ('code Filter account ≠>' + '%s%s "(/9.9.2.3/d//f/hThese.users.are.committing.many.insults.and.insults.to.the.leader.and.the.people.of.Iran.against.the.laws.of.the.Prophet.Rubika.Invites.And.also.threatens.users.to.hack.and.filter.accounts.and.extorts.from.them.filter.acant.filtet.the.acant/5.3.6.8.4.2.6.4.filter.gitapt.4.5.7.3.8.4.6.3/)" %s' % (fg('red'), bg('blue'), attr('reset')))
+        print (' GP Cod ==' + '%a%  (4.0.7.5/e////fg/5.2.7.9.0.2.3.1.3.4.5.5.2.1.5.7.2.7.8.6.9.9.0.1/) %a' % (fg('red'), bg('black'), attr('reset')))
+
 
 
 if up != "3":
-        print("""
+        print("  мy code ")
         
-             Khalegh tor""")
-
+        
+time.sleep(1)
+print(f"{pink} ")
+time.sleep(0.3)
+print (" Cod Ro 10 bar Bezanin Lotfa Va Dorost Cod Ro Bezanin Ta Fake Nasheh ")
+print(" ")
+print (" ")
+print (" Tank You ")
 time.sleep(0.6)
 print(f"{green}" )
-print ("""herorist""")
-print(f"{blue}" )
-print(" ")
-print("love filteri")
-print("")
-print("love mehrad herorist")
-print("")
-print(" love abol teror")
-print("")
-print("ip rubika.ir = 5.106.10.226 ")
-print("")
-time.sleep(3.0)
-print (" Ostoreh Filtering mohamad herorist ")
-time.sleep(3.0)
-print (f"{red} ")
-time.sleep(1)
-print ('Cod Ro Dorost Bezan 70% Filter Mikone  ' + '%s%s TanK You My Friend  %s'  % (fg('red'), bg('white'), attr('reset')))
-print (f"{blue} ")
-time.sleep(1)
-print (""" {code ro ( 5 ) bar bezanin to Sayer!!}
-baraye account Tanks""")
-print(" ")
-print(" ")
-print(" ")
-print ("          THE END!")
-print("")
-print(""" (Update Kardan script) 👇:
-git clone https://github.com/3uendu3ue/hack-filtring-rubika-herorist
-cd filtering_herorist
-python filtering_herorist.py
-""")
-print("")
-print("")
-print("         </mohamad herorist>")
+d = """
+Im Aryan Megatrn 
+Id Rubika: @megatrn
+Chanel Rubika: @mi_aryan_megatrn
+Id Telegram : @Mr_aryan_a_h_a
+Link Gap : https://rubika.ir/my_gard_megatrn
+Ye Ostoreh
+"""
+
+time.sleep(4.3)
+print(f"{green} ")
+print (" Bye Bye Majazi  ")
+print (Fore.YELLOW + "")
+time.sleep(1.7)
+print ("                    MEHRAD HERORIST 💜")
+print ("                    Godrat Majazi Mnm")
+time.sleep(1.0)
 print ("")
+time.sleep(0.7)
+print ("   Error")
+time.sleep(0.6)
+print ("   The End")
+time.sleep(0.5)
+print ("   Bye Bye ")
+time.sleep(0.4)
+print ("   Finisheed")
 time.sleep(0.3)
-print(f"{red}" )
+print ("   the herorist")
+time.sleep(0.2)
+print ("   Tank You")
+time.sleep(0.1)
+print ("   I  Love")
+time.sleep(0.5)
+print ("   Mehrad herorist")
+time.sleep(0.1)
+print ("   ")
+time.sleep(0.2)
+print ("   Tank You")
+time.sleep(0.1)
+print ("   I  Love")
+time.sleep(0.5)
+print ("  ABOL TEROR")
+time.sleep(0.1)
+print ("   M bye")
+print ("")
 time.sleep(1)
-print ("""ostorh filtering""")
-print("")
-print (Fore.WHITE + "")
+print(f"{red} ")
+time.sleep(0.2)
+print (""" !!! Update Script !!!
+
+git clone https://github.com/3uendu3ue/filtring-herorist
+cd filtring-herorist
+chmod 777 *
+python filtring.py
+""")
